9f9d3e9470892401990782921380ac27f3463b3d
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-01-30 23:11:31 +1100

    First working brew formula

---
+++ aliases.rb
@@ -0,0 +1,28 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Aliases < Formula
+  desc "Bash aliases on steroids"
+  homepage "https://github.com/sebglazebrook/aliases"
+  url "https://github.com/sebglazebrook/aliases/raw/master/releases/0.1.0/mac/aliases.tar.gz"
+  version "0.1.0"
+  sha256 "7bad9f02ddd331ba9dffa414c87e6e4fa87d839296de0dec9a1797e118df7221"
+
+  def install
+    bin.install "aliases"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test aliases`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
