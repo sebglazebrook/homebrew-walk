9204ce2468ed9367371d535bbcd2871b3c30ed5e
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-05-19 21:25:48 +1000

    Version bump 0.7.2

--- aliases.rb
+++ aliases.rb
@@ -5,9 +5,9 @@
 class Aliases < Formula
   desc "Bash aliases on steroids"
   homepage "https://github.com/sebglazebrook/aliases"
-  url "https://github.com/sebglazebrook/aliases/raw/master/releases/0.7.1/mac/aliases.tar.gz"
-  version "0.7.1"
-  sha256 "817aef272dfa4d31d38293cbd7b33e429dab5558d98458929a57e5553e19257f"
+  url "https://github.com/sebglazebrook/aliases/raw/master/releases/0.7.2/mac/aliases.tar.gz"
+  version "0.7.2"
+  sha256 "5f70923a006765577b79077235fcc27d94f668e5ed8f4520075c474d0fb95ba6"
 
   def install
     bin.install "aliases"
